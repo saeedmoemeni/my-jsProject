eadbd6252e70abdfddc6bac2dc9d51df72d875c1
saeed <momenism17@gmail.com>
2025-02-04 18:15:47 +0330

    ex

---
+++ CAPTCHA EX/index.js
@@ -0,0 +1,4 @@
+var firstRandomNumber = Math.random() * 100000
+var finalCode = Math.floor(firstRandomNumber)
+
+console.log(finalCode)
--- tamrin/index.js
+++ tamrin/index.js
@@ -1,10 +1,6 @@
-var username = prompt("enter your username : ")
-var password = prompt("enter your password : ")
-
-if(username.length < 3 || password.length < 8  ){
-
-    alert("یوزرنیم باید حداقل سه کاراکتر و پسوورد حداقل بایذ هشت کاراکتر باشد ")
-
-} else {
-    alert("ثبت نام شما با موفقیت انجام شد ")
+var username = prompt("enter your username please : ")
+if( username.toLowerCase() == "ali"){
+    alert("its oke")
+}else{
+    alert("not ok")
 }
